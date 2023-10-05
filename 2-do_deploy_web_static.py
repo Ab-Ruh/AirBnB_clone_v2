f2367e122ff6854c762c214507aec6f7f5b12919
Ab-Ruh <abell.demere@gmail.com>
2023-10-05 21:45:39 +0300

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,65 +3,36 @@
 Fabric script to distribute and deploy an archive to web servers
 """
 
-import os
+from fabric.api import *
 from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
+from os.path import exists
+import argparse
 
-
-env.hosts = ['34.138.32.248', '3.226.74.205']
-
-
-def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+env.hosts = ['100.24.74.170', '18.234.80.72']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """Deploys the static files to the host servers.
-    Args:
-        archive_path (str): The path to the archived static files.
-    """
-    if not os.path.exists(archive_path):
+    '''
+    this method distributes the archive
+    '''
+    if not exists(archive_path):
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
+
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version deployed!')
-        success = True
-    except Exception:
-        success = False
-    return success
+        archive_name = archive_path.split('/')[-1]
+        archive_no_ext = archive_name.split('.')[0]
+        target_path = "/data/web_static/releases/" + archive_no_ext
+
+        put(archive_path, '/tmp', use_sudo=True)
+        run('sudo mkdir -p {}'.format(target_path))
+        run('sudo tar -xzf /tmp/{} -C {}'.format(archive_name, target_path))
+        run('sudo rm /tmp/{}'.format(archive_name))
+        run('sudo mv {}/web_static/* {}/'.format(target_path, target_path))
+        run('sudo rm -rf {}/web_static'.format(target_path))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(target_path))
+        return True
+    except Exception as e:
+        return False
