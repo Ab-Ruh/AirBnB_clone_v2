2a01f36be0509c5d0d30ec969352fc1ab83d89e8
Ab-Ruh <abell.demere@gmail.com>
2023-10-22 17:12:32 +0300

    flask

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -3,40 +3,38 @@
 Flask web application to display an HBNB-like page.
 """
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-
-app = Flask(__name)
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """
-    Display an HBNB-like page with States, Cities, Amenities, and Places.
-    """
-    states = storage.all(State).values()
-    cities = storage.all(City).values()
-    amenities = storage.all(Amenity).values()
-    places = storage.all(Place).values()
-
-    return render_template('100-hbnb.html',
-                           states=states,
-                           cities=cities,
-                           amenities=amenities,
-                           places=places)
+app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """
-    Remove the current SQLAlchemy Session after each request.
-    """
+def dispose(exception):
+    '''
+    Remove session
+    '''
     storage.close()
 
 
-if __name__ == "__main__":
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_filters():
+    '''
+    display filters
+    '''
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    places = storage.all(Place)
+    data = {
+        'states': list(states.values()),
+        'amenities': list(amenities.values()),
+        'places': list(places.values())
+    }
+    return render_template('100-hbnb.html', **data)
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
 """This script starts a Flask web application"""
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-def states_list():
-    """Display a list of states from the database."""
-    states = storage.all(State).values()
-    return render_template('9-states.html', state=None, states=states)
-
-
-@app.route('/states/<state_id>', strict_slashes=False)
-def state_cities(state_id):
-    """Display a list of cities for a specific state from the database."""
-    state = storage.get(State, state_id)
-    return render_template('9-states.html', state=state)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Close the current SQLAlchemy session."""
+def dispose(exception):
+    '''
+    current session
+    '''
     storage.close()
+
+
+@app.route('/states/')
+@app.route('/states/<id>', strict_slashes=False)
+def states_with_id(id=None):
+    '''
+    list of state
+    '''
+    if id:
+        id = 'State.{}'.format(id)
+    return render_template('9-states.html', states=storage.all(State), id=id)
 
 
 if __name__ == '__main__':
