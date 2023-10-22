f1d373d80924f587cfa89268605db5d1248c2962
Ab-Ruh <abell.demere@gmail.com>
2023-10-22 15:48:48 +0300

    read

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """This script starts a Flask web application"""
 
-from flask import Flask, render_template
 from models import storage
-from models.state import State
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
+@app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
     """Display a list of states and their cities from the database."""
-    states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states)
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown(exc):
     """Close the current SQLAlchemy session."""
     storage.close()
 
